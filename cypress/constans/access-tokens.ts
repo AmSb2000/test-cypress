898ed510a24da778eadec21186acf22c8961d13c
sadegh <habibian@sadegh>
2023-05-27 20:33:07 +0330

    Add constance values for login, and rest tests

---
+++ cypress/constans/access-tokens.ts
@@ -0,0 +1,5 @@
+export const ACCESS_TOKEN = {
+  admin: "",
+  sazande: "",
+  karmand: "",
+}
---
+++ cypress/constans/login-rest-data.ts
@@ -0,0 +1,14 @@
+export const LOGIN_REST_DATA = {
+  admin: {
+    username: 'admin',
+    password: 'admin'
+  },
+  sazande: {
+    username: 'sazande',
+    password: 'admin'
+  },
+  karmand: {
+    username: 'karmand',
+    password: 'admin'
+  }
+}
